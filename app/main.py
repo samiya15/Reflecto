e297779b281ed6171a8c9686074503265dd8638a
fatumaahmed2024 <Fatuma.Mohamed@strathmore.edu>
2025-07-04 13:42:22 +0300

    connection

--- app/main.py
+++ app/main.py
@@ -1,23 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from sentiment_analysis import analyze_sentiment
 from profanity_filter import check_profanity
 
 app = FastAPI()
 
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class FeedbackInput(BaseModel):
     message: str
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Reflcto API!"}
+    is_anonymous: bool
 
 @app.post("/feedback/analyze")
 def analyze_feedback(data: FeedbackInput):
     sentiment_result = analyze_sentiment(data.message)
     profanity_result = check_profanity(data.message)
-
-    return {
-        **sentiment_result,
-        **profanity_result
-    }
+    return {**sentiment_result, **profanity_result, "is_anonymous": data.is_anonymous}
